00faff8d7e5dbc71097fe2b933d467f9ffee3f20
Matthew O'Mara <omara.matthew1@gmail.com>
2021-06-09 22:54:19 -0400

    add reset and resize buttons

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 window.addEventListener("load", appInit, true); function appInit() {
-
     drawGrid();
     changePixelColor();
 
+    document.getElementById('reset-btn').addEventListener('click', () => {
+        clearGrid();
+    });
+
+    document.getElementById('resize-btn').addEventListener('click', () => {
+        resizeGrid();
+    });
 };
 
 function changePixelColor() {
     const pixels = document.querySelectorAll('.sketch-pixel');
-
     pixels.forEach(pixel => {
         pixel.addEventListener('mouseover', (e) => {
             e.target.style.backgroundColor = "orange";
@@ -15,12 +20,9 @@ function changePixelColor() {
     });
 }
 
-function drawGrid() {
-    let i = 0;
-    let j = 0;
-
-    for (i = 1; i <= 16; i++) {
-        for (j = 1; j <= 16; j++) {
+function drawGrid(size = 16) {
+    for (let i = 1; i <= size; i++) {
+        for (let j = 1; j <= size; j++) {
             let d = document.createElement('div');
             d.classList.add('sketch-pixel');
             document.getElementById('sketch-container').appendChild(d);
@@ -28,3 +30,13 @@ function drawGrid() {
     }
 }
 
+function clearGrid() {
+    const pixels = document.querySelectorAll('.sketch-pixel');
+    pixels.forEach(pixel => pixel.style.backgroundColor = "white");
+}
+
+function resizeGrid() {
+    
+    let size = prompt("Choose new gride size 16-100:");
+    drawGrid(size);
+}
